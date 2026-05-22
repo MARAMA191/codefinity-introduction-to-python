650569cd2591b5a271703c7f9731526122e2e1e6
MARAMA191 <>
2026-05-22 05:05:18 +0000

    Conditionals

---
+++ conditional_statements/conditionals/main.py
@@ -0,0 +1,15 @@
+# Input variables
+product_type = 'Dairy' 
+day_of_week = 'Wednesday
+if product_type = Fruits:
+	if day_of_week= "Monday":
+	print("10% discount on fruits today")
+elif product_type = "Vegetables":
+	if day_of_week = "Tuesday":
+	print("15% discount on 		vegetables today")
+elif product_type = "Dairy":
+	if day_of_week = "Wednesday":
+	print("20% discount on dairy today")
+elif product_type = "Other"	:
+	print("No discount available")
+else print("No special discount today")
