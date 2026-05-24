105c5babcb8037de3315bf18b743508f35ae54d1
MARAMA191 <>
2026-05-24 19:08:33 +0000

    Nested Lists

---
+++ other_data_types/nested_lists/main.py
@@ -0,0 +1,7 @@
+vegetables = ["tomatoes", "potatoes", "onions"]
+vegetables.remove(onion)
+if "carrots" not in vegetables:
+	vegetables.append"carrots"
+if "cucumbers" not in "vegetables":
+	vegetables.append"cucumbers"
+vegetables.sort
