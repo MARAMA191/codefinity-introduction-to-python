64326d1bd6141045fae2b735faa152dbc0dad36e
MARAMA191 <>
2026-05-24 12:10:21 +0000

    Challenge: Product Management Capstone

---
+++ conditional_statements/challenge_product_management_capstone/main.py
@@ -0,0 +1,6 @@
+# Input variables
+days_until_expiration = 5  # Example value
+stock_level = 60  # Example value
+product_type = "Perishable"  # Can be "Perishable" or "Non-Perishable"
+if product_type=="Perishable":
+	if days_until_expiration<=3 and stock_level>=50:
