f56a4025918de8a6cab0a32cc002827dac2715de
MARAMA191 <>
2026-05-27 15:00:14 +0000

    Dictionaries

--- other_data_types/dictionaries/main.py
+++ other_data_types/dictionaries/main.py
@@ -1,6 +1,13 @@
-groceruy_inventory={
-	"Milk":(113,"Dairy")
-	"Eggs":(116,"Dairy")
-	"Bread":(117,"Bakery")
+grocery_inventory={
+	"Milk":(113,"Dairy"),
+	"Eggs":(116,"Dairy"),
+	"Bread":(117,"Bakery"),
 	"Apple":(141,"Produce")
 }
+bread_details= grocery_inventory.get("Bread")
+print(Detaiks of Bread",["Brad"])
+grocery_inventory.update({"Cookies":(143,"Bakery")})
+grocery_inventory.pop("Eggs")
+print("Details of Bread:",["Bread"])
+print("Inventory after adding Cookies:",grocery_inventory)
+print("Inventory after removig Eggs", '{)
