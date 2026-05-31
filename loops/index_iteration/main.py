c3bda62aad4d2c146fdce73543f551daf5d5a801
MARAMA191 <>
2026-05-31 01:04:35 +0000

    Index iteration

--- loops/index_iteration/main.py
+++ loops/index_iteration/main.py
@@ -1,4 +1,5 @@
 prices = [29.99, 45.50, 12.75, 38.20]
-discounts=[10%,20%,15%,5%]]
-for i in range(len(prices))
-	
+discounts=[0.10,0.20,0.15,0.05]
+for i in range(len(prices)):
+	new_price =prices[i]*(1-discounts[i])
+	print(f"Updated price for index {i}: new_price[i]")
