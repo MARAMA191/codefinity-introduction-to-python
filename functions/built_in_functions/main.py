86d492e72320dc55e1e0e50c570fc1b291718c1e
MARAMA191 <>
2026-06-01 02:24:17 +0000

    Built-in Functions

---
+++ functions/built_in_functions/main.py
@@ -0,0 +1,19 @@
+# Dictionary of products with price and quantity sold as strings
+products = {
+    "Apple": ["1.20", "50"],   # "Item": [price, quantity sold]
+    "Banana": ["0.50", "100"],
+    "Cherry": ["2.50", "25"],
+    "Mango": ["1.75", "40"]
+}
+total_sales_list=[]
+for fruit,infos in products.items():})
+	price= float(infos[0])
+	print(price)
+	quantity_sold=int(infos[1])
+	total_sales= quantity_sold*price
+	print(f"Total sales for {fruit}: total_sales
+	total_sales_list.append [total_sales]
+	total_sum=sum [total_sales_list]
+	min_sales=min [total_sales_list]
+	max_sales=max [total_sales_list]
+	
